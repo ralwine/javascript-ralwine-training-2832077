59dcc3b944294484322fd12efd33bc99c0b1ea58
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-11-14 15:12:44 -0600

    completed next set of exercises

---
+++ 03_09exercises!/script.js
@@ -0,0 +1,16 @@
+const cabinet = {
+    name: "Old Cabinet",
+    material: 'walnut',
+    drawer1: {
+        item1: 'map',
+        item2: 'eyeglass'
+    }, 
+    drawer2: {
+        item1: 'bundle of shoelaces',
+        item2: 'scissors'
+    }
+}
+
+console.log(cabinet)
+console.log(cabinet.name)
+console.log(cabinet.drawer1.item2)
